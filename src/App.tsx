12a80e3b8534852a205620e137cebcff62631740
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2024-03-19 01:19:02 +0200

    modal

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import Modal from "./components/modal/Modal";
 
 function App() {
   return (
-    <>
+    <div className="relative">
       <div className="flex flex-col w-200 px-20 py-10">
         <AddForm />
         <ListPost />
       </div>
       <Modal />
-    </>
+    </div>
   );
 }
 
--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,5 +1,17 @@
-const Modal = () => {
-  return <div>Modal</div>;
+import { IoClose } from "react-icons/io5";
+
+const Modal = ({ children }) => {
+  return (
+    <>
+      <div className="flex w-screen h-screen opacity-50 bg-slate-500 absolute left-0 top-0 "></div>
+      <div className="flex w-60 h-60 bg-white absolute top-100 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <button className="absolute right-3 top-3 border-2 border-black rounded-full">
+          <IoClose className="" />
+        </button>
+        {children}
+      </div>
+    </>
+  );
 };
 
 export default Modal;
