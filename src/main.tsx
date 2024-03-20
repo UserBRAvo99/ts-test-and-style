fc4d82fe96e77f1cd22b836d34011aa58ad8ea01
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2024-03-20 23:51:09 +0200

    redux persist

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
 
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { Provider } from 'react-redux'
-import { store } from './redux/store.ts'
+import "./index.css";
 
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   // огортаємо наш проект в провайдер
   <Provider store={store}>
-    <App />
-  </Provider>,
-)
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,17 +1,45 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { postsReducer } from "./post/slice";
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 // імпортуємо конфігур стор
 
+const persistConfig = {
+  key: "post",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, postsReducer);
+
 export const store = configureStore({
-    reducer: {
-        posts: postsReducer
-    }
-})
+  reducer: {
+    posts: persistedReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export let persistor = persistStore(store);
 
 // redux toolkit - забераємо з документації , для показу данних в редаксі(для покращення комунікації)
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
